795861f0dfb7577d38280f072f86dfcf5fcbfd91
Manuel Solórzano <manuel.solorzanoc@gmail.com>
2012-10-01 14:49:22 -0500

    una mas

---
+++ src/inputdata/Conexion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inputdata;
+
+/**
+ *
+ * @author msolorzano
+ */
+public class Conexion {
+    Aeropuerto  aeropuertoInicial;
+    Aeropuerto  aeropuertoFinal;
+    double      distancia;
+    boolean     conectado;
+}
