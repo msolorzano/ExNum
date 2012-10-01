5f7e5e9b8ab04b110994979152ad03f87510b556
miguelavg <miguelavg@chevere>
2012-09-30 20:45:34 -0500

    sd

---
+++ src/inputdata/Serializer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inputdata;
+
+/**
+ *
+ * @author miguelavg
+ */
+public class Serializer {
+    
+}
