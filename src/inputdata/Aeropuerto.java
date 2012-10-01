27f94c7d790204e1b16439cb1481d09bfb5f00d6
miguelavg <miguelavg@chevere>
2012-09-30 21:17:50 -0500

    sd

--- src/inputdata/Aeropuerto.java
+++ src/inputdata/Aeropuerto.java
@@ -16,4 +16,13 @@ public class Aeropuerto {
     public boolean principal;
     public String ciudad;
     
+    public Aeropuerto(int x, int y, int capacMax, int capacActual, boolean principal, String ciudad){
+        this.x = x;
+        this.y = y;
+        this.capacMax = capacMax;
+        this.capacActual = capacActual;
+        this.principal = principal;
+        this.ciudad = ciudad;
+    }
+    
 }
