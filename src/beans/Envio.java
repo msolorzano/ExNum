f143b6ad230821fb05f5bcb02f4bf65f4900574d
Manuel Solórzano <manuel.solorzanoc@gmail.com>
2012-10-02 20:14:06 -0500

    Terminando de hacer lo anterior

---
+++ src/beans/Envio.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.util.Date;
+
+/**
+ *
+ * @author msolorzano
+ */
+public class Envio {
+    int idAeropuertoInicio;
+    int idAeropuertoFin;
+    Date fecha;
+    
+}
